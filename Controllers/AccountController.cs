cbe55bc3c7920f3ca9a80f3bdcbe8e73ec597636
Mete Sayan <metesayan99@gmail.com>
2023-11-29 23:29:47 +0300

    Login & Logout

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -20,6 +20,39 @@ namespace IdentityAppWithMVC.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            LoginViewModel vm = new LoginViewModel();
+            vm.ReturnUrl = returnUrl ?? Url.Content("~/");
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(
+                    loginViewModel.UserName
+                    , loginViewModel.Password
+                    , loginViewModel.RememberMe
+                    , lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(loginViewModel);
+                }
+            }
+            return View(loginViewModel);
+        }
+
         public async Task<IActionResult> Register(string? returnUrl = null)
         {
             RegisterViewModel registerViewModel = new RegisterViewModel();
@@ -32,9 +65,9 @@ namespace IdentityAppWithMVC.Controllers
         {
             registerViewModel.ReturnUrl = returnUrl;
             returnUrl = returnUrl ?? Url.Content("~/");
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                var user = new AppUser { Email = registerViewModel.Email, UserName = registerViewModel.UserName};
+                var user = new AppUser { Email = registerViewModel.Email, UserName = registerViewModel.UserName };
                 var result = await _userManager.CreateAsync(user, registerViewModel.Password);
                 if (result.Succeeded)
                 {
@@ -45,6 +78,14 @@ namespace IdentityAppWithMVC.Controllers
             }
 
             return View(registerViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
