3431832a7aa7ce6c61183f29a8b37064b35100c5
Mete Sayan <metesayan99@gmail.com>
2023-12-30 23:42:41 +0300

    Custom Policies

---
+++ Authorization/NicknameRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityAppWithMVC.Authorization
+{
+    public class NicknameRequirement : IAuthorizationRequirement
+    {
+        public string Name { get; set; }
+        public NicknameRequirement(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Authorization/OnlyPokemonAuthorization.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityAppWithMVC.Authorization
+{
+    public class OnlyPokemonAuthorization : AuthorizationHandler<OnlyPokemonAuthorization>, IAuthorizationRequirement
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OnlyPokemonAuthorization requirement)
+        {
+            if (context.User.IsInRole("Pokemon"))
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
